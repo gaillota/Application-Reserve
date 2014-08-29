ad00ecf25011bc10ba97a3a3e426973476ad1a13
na-ji <naji.demolitionman@gmail.com>
2014-08-29 23:41:42 +0200

    Set UserBundle parent

--- src/Ferus/UserBundle/FerusUserBundle.php
+++ src/Ferus/UserBundle/FerusUserBundle.php
@@ -6,4 +6,11 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class FerusUserBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
 }
